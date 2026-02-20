4db80b9a296505608f9a22cbe781b405d7df0773
IamKaleb21 <hionta16@gmail.com>
2026-02-20 01:56:15 -0500

    chore(blog): add @astrojs/mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import icon from 'astro-icon';
 
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,5 +23,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [icon(), react(), sitemap()]
+  integrations: [icon(), react(), sitemap(), mdx()]
 });
